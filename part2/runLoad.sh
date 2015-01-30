82c5345c8b10307fb74b722b6d2683aa4014b76b
naren <nnaren1902@gmail.com>
2015-01-29 21:18:29 -0800

    complete project 2

---
+++ part2/runLoad.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mysql CS144 < drop.sql
+
+mysql CS144 < create.sql
+
+ant -f build.xml run-all
+
+sort -u categories.csv > categories.dat
+sort -u items.csv > items.dat
+sort -u users.csv > users.dat
+sort -u bids.csv > bids.dat
+
+mysql CS144 < load.sql
+
+rm *.csv 
+rm *.dat
+
